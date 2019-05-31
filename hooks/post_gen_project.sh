86bf8a58810ad725c430b917719b9b33e298c43b
khchine5 <hamzakhchine@gmail.com>
2019-05-31 16:22:14 +0100

    More changes

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -10,7 +10,7 @@ USERGROUP=www-data
 sudo chown :$USERGROUP .
 sudo chmod g+wx .
 
-virtualenv $ENVDIR
+virtualenv $ENVDIR --python=python3
 . $ENVDIR/bin/activate
 
 pip install -U pip
@@ -30,3 +30,21 @@ pip install -e lino
 git clone https://github.com/lino-framework/xl.git
 pip install -e xl
 {% endif %}
+
+apt-get update && \
+    apt-get upgrade -y && \ 	
+    apt-get install -y \
+	git \
+	python3 \
+	python3-dev \
+	python3-setuptools \
+	python3-pip \
+	nginx \
+	supervisor \
+	sqlite3 && \
+	pip3 install -U pip setuptools
+apt install -y libreoffice python3-uno tidy swig graphviz
+libreoffice '--accept=socket,host=127.0.0.1,port=8100;urp;' & 
+apt-get install redis-server ; redis-server &
+pip3 install -e svn+https://svn.forge.pallavi.be/appy-dev/dev1#egg=appy
+pip3 install uwsgi
