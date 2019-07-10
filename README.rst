ec3ceae8961288db491b6c250dd7ae96c5320429
khchine5 <hamzakhchine@gmail.com>
2019-07-10 10:44:07 +0100

    #3036 Update cookiecutter

--- README.rst
+++ README.rst
@@ -1,1 +1,8 @@
 # cookicutter-startsite
+The cookicutter is not intend to be used as it is. This is part of the _getlino: https://github.com/lino-framework/getlino/ script, the recommended way to install Lino both for Dev or Prod.
+.. code-block:: python
+   :emphasize-lines: 3,5
+    
+    sudo pip install getlino
+    sudo getlino.py setup # to install lino in your fresh machine.
+    sudo getlino.py startsite # to start a new Lino site.
--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -10,14 +10,6 @@ USERGROUP=www-data
 sudo chown :$USERGROUP .
 chmod g+wx .
 
-pip3 install virtualenv
-
-virtualenv $ENVDIR --python=python3
-. $ENVDIR/bin/activate
-
-pip3 install -U pip
-pip3 install -U setuptools
-
 mkdir $REPOSDIR
 cd $REPOSDIR
 
@@ -41,30 +33,4 @@ git clone https://github.com/lino-framework/xl.git
 pip install -e xl
 {% endif %}
 
-sudo apt-get update && \
-    apt-get upgrade -y && \ 	
-    apt-get install -y \
-	git \
-	python3 \
-	python3-dev \
-	python3-setuptools \
-	python3-pip \
-	nginx \
-	supervisor \
-	sqlite3 && \
-	pip3 install -U pip setuptools
-sudo apt install -y libreoffice python3-uno tidy swig graphviz
-libreoffice '--accept=socket,host=127.0.0.1,port=8100;urp;' & 
-sudo apt-get install redis-server ; redis-server &
-pip3 install -e svn+https://svn.forge.pallavi.be/appy-dev/dev1#egg=appy
-pip3 install uwsgi
-
-{% if cookiecutter.db_engine == "mysql" %}
-sudo apt install mysql-server libmysqlclient-dev python-dev libffi-dev libssl-dev
-pip3 install mysqlclient
-{% else  %}
-sudo apt install postgresql postgresql-contrib 
-pip3 install psycopg2-binary
-{% endif  %}
-
-python {{ cookiecutter.prjname }}/manage.py prep --noinput
+python $REPOSDIR/{{ cookiecutter.prjname }}/manage.py prep --noinput
